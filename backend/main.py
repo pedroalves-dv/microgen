20649a7fea1d1a55879c74de34d1d4a582707e27
pedroalves-dv <pedroalves.dv@gmail.com>
2026-02-26 01:50:01 +0100

    CORS url updated with vercel url (frontend deployment) in main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ if API_KEY:
 app = FastAPI(title="SEO Brief Generator")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://microgen.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
